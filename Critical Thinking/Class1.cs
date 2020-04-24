ea00de230fae831db0411f4f7c613fa5e9d7666b
unknown <carter53202@gmail.com>
2020-04-24 13:08:13 -0500

    Made a new class to run the method RunLoop in. Loop is set to run 5 times and to output the integer.

---
+++ Critical Thinking/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Critical_Thinking
+{
+    class Class1
+    {
+
+
+        //Member Variables (HAS A)
+
+        //Constructor
+
+        //Member Methods (CAN DO)
+        public void RunLoop() 
+        { 
+            for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine(i);
+                Console.ReadLine();
+          
+            }
+        
+        }
+
+
+    }
+}
---
+++ Critical Thinking/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Critical_Thinking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 test = new Class1();
+            test.RunLoop();
+        }
+    }
+}
